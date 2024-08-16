2c9bd4e6539f29be85a68f83b1c1c950d9bcba91
Merin Muhammad <merin1970@gmail.com>
2024-08-16 17:25:40 +0100

    added space_decay dataset
    https://www.kaggle.com/datasets/kandhalkhandeka/satellites-and-debris-in-earths-orbit?resource=download

--- space_debris/main.py
+++ space_debris/main.py
@@ -1,8 +1,15 @@
+import csv
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
 
-ax = plt.axes(projection="3d")
-ax.scatter(3,5,7)
-plt.show()
+# ax = plt.axes(projection="3d")
+# ax.scatter(3,5,7)
+# plt.show()
+
+
+with open('space_debris\space_decay.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        print(', '.join(row))
