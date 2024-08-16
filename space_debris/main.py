eda682eb8cf3c323b398256a2da6d1fc16cb64ec
Merin Muhammad <merin1970@gmail.com>
2024-08-16 18:16:20 +0100

    added sgp4 to calculate space object position

--- space_debris/main.py
+++ space_debris/main.py
@@ -1,15 +1,38 @@
 import csv
+
+from sgp4.api import Satrec
+from sgp4.api import jday
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
 
-# ax = plt.axes(projection="3d")
-# ax.scatter(3,5,7)
-# plt.show()
+# Example TLE data
+tle_line1 = "1 26741U 92072J   21304.94919376  .00000883  00000-0  24341-1 0  9992"
+tle_line2 = "2 25544  51.6435 324.2024 0007223  13.8584  64.2474 15.48910339303824"
+
+# Create a satellite object
+satellite = Satrec.twoline2rv(tle_line1, tle_line2)
+
+# Specify the observation time
+year, month, day, hour, minute, second = 2021, 10, 2, 12, 0, 0
+jd, fr = jday(year, month, day, hour, minute, second)
+
+# Compute the position and velocity
+e, r, v = satellite.sgp4(jd, fr)
+
+# r contains the position in kilometers [x, y, z]
+print(f"Position (km): {r}")
 
 
-with open('space_debris\space_decay.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
+
+ax = plt.axes(projection="3d")
+ax.scatter(r[0], r[1], r[2])
+plt.show()
+
+
+# with open('space_debris\space_decay.csv', newline='') as csvfile:
+#     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+#     for row in spamreader:
+#         print(', '.join(row))
