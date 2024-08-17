fe6a942d5ada06b1bf93df8c0660f00ec4d58f06
Merin Muhammad <merin1970@gmail.com>
2024-08-17 20:30:49 +0100

    reorganized classes for reusability

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 from model import *
 from camera import Camera
 from light import Light
+from mesh import Mesh
 
 class GraphicsEngine:
     def __init__(self, win_size=(1600, 900)):
@@ -33,13 +34,15 @@ class GraphicsEngine:
         self.light = Light()
         #camera
         self.camera = Camera(self)
+        #mesh
+        self.mesh = Mesh(self)
         #scene
         self.scene = Cube(self)
 
     def check_events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
-                self.scene.destroy()
+                self.mesh.destroy()
                 pg.quit()
                 sys.exit()
 
---
+++ texture.py
@@ -0,0 +1,19 @@
+import pygame as pg
+import moderngl as mgl
+
+
+class Texture:
+    def __init__(self, ctx):
+        self.ctx = ctx
+        self.textures = {}
+        self.textures[0] = self.get_texture(path='textures/metallic-textured-background.jpg')
+
+    def get_texture(self, path):
+        texture = pg.image.load(path).convert()
+        texture = pg.transform.flip(texture, flip_x=False, flip_y=True)
+        texture = self.ctx.texture(size=texture.get_size(), components=3,
+                                   data=pg.image.tostring(texture, 'RGB'))
+        return texture
+    
+    def destroy(self):
+        [tex.release() for tex in self.texture.values()]
